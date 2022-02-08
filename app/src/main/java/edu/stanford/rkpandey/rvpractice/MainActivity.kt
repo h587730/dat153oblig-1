5e1cfe150cb0d51323a0afae8fa63bd1ac78cf6f
Lang Awuor <langawuor@gmail.com>
2022-02-08 10:02:46 +0100

    second commit

---
+++ app/src/main/java/edu/stanford/rkpandey/rvpractice/MainActivity.kt
@@ -0,0 +1,86 @@
+package edu.stanford.rkpandey.rvpractice
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.Serializable
+
+private const val TAG1 = "onCreateOptionsMenu"
+private const val TAG2 = "onOptionsItemSelected"
+
+data class Person(val name: String, val age: Int) : Serializable
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: ContactsAdapter
+    private lateinit var contacts: MutableList<Person>
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        // 6 STEPS OF IMPLEMENTING RecyclerView, from https://guides.codepath.com/android/using-the-recyclerview
+        // 1. Add RecyclerView AndroidX library to the Gradle build file - DONE
+        // 2. Define a model class to use as the data source - DONE
+        contacts = createContacts()
+        // 3. Add a RecyclerView to your activity to display the items - DONE
+        // 4. Create a custom row layout XML file to visualize the item - DONE
+        // 5. Create a RecyclerView.Adapter and ViewHolder to render the item
+        adapter = ContactsAdapter(this, contacts)
+        rvContacts.adapter = adapter
+        // 6. Bind the adapter to the data source to populate the RecyclerView
+        rvContacts.layoutManager = LinearLayoutManager(this)
+    }
+    
+    private fun createContacts(): MutableList<Person> {
+        val contacts = mutableListOf<Person>()
+        for (i in 1..100) {
+            contacts.add(Person("Person $i", i))
+        }
+        return contacts
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        Log.i(TAG1, "Menu created")
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //Add Picture to database
+        if (item.itemId == R.id.miAdd) {
+            // Navigate to ContactsActivity
+            val intent = Intent(this, ContactsActivity::class.java)
+            startActivityForResult(intent, 42)
+            Log.i(TAG2, "Add picture selected")
+            return true
+        }
+        //Go to quiz
+        if (item.itemId == R.id.miQuiz) {
+            // Navigate to ContactsActivity
+            val intent = Intent(this, QuizActivity::class.java)
+            startActivityForResult(intent, 42)
+            Log.i(TAG2, "Quiz item selected")
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 42 && resultCode == Activity.RESULT_OK) {
+            val newPerson = data?.getSerializableExtra("person") as Person
+            contacts.add(0, newPerson)
+            adapter.notifyDataSetChanged()
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+}
